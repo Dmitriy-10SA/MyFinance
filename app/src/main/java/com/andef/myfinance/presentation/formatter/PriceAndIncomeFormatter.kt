f52d088937212e54aa4b3460ac187f8274197416
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-02-19 11:32:52 +0400

    Functional

--- app/src/main/java/com/andef/myfinance/presentation/formatter/PriceAndIncomeFormatter.kt
+++ app/src/main/java/com/andef/myfinance/presentation/formatter/PriceAndIncomeFormatter.kt
@@ -2,6 +2,6 @@ package com.andef.myfinance.presentation.formatter
 
 object PriceAndIncomeFormatter {
     fun formatPrice(price: Double): String {
-        return "%.2f".format(price)
+        return "%.2f".format(price) + "₽"
     }
 }
