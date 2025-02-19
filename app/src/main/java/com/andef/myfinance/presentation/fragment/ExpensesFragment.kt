a6ce9b710824831dc1572217d03c06e603718776
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-02-19 14:03:54 +0400

    Functional

--- app/src/main/java/com/andef/myfinance/presentation/fragment/ExpensesFragment.kt
+++ app/src/main/java/com/andef/myfinance/presentation/fragment/ExpensesFragment.kt
@@ -3,7 +3,6 @@ package com.andef.myfinance.presentation.fragment
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
