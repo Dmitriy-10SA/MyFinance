3d79c6447d9d55784a816ef68abd77886fb0f67d
dsemkin <semkin_dmitriy10@vk.com>
2025-08-30 09:53:00 -0700

    Expenses repos

---
+++ composeApp/src/commonMain/kotlin/com/andef/myfinance/core/data/expense/repository/ExpenseRepositoryImpl.kt
@@ -0,0 +1,75 @@
+package com.andef.myfinance.core.data.expense.repository
+
+import com.andef.myfinance.core.data.expense.dao.ExpenseDao
+import com.andef.myfinance.core.data.expense.mapper.ExpenseMapper
+import com.andef.myfinance.core.domain.expense.entities.ExpenseModel
+import com.andef.myfinance.core.domain.expense.repository.ExpenseRepository
+import com.andef.myfinance.core.domain.expense_category.entities.ExpenseCategoryModel
+import com.andef.myfinance.core.utils.mappers.localDateToInt
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.datetime.LocalDate
+
+class ExpenseRepositoryImpl(
+    private val dao: ExpenseDao,
+    private val mapper: ExpenseMapper
+) : ExpenseRepository {
+    override suspend fun getExpenseById(id: Long): ExpenseModel {
+        return mapper.toEntity(dao.getExpenseById(id))
+    }
+
+    override suspend fun addExpense(expenseModel: ExpenseModel) {
+        dao.insert(mapper.fromEntity(expenseModel))
+    }
+
+    override suspend fun updateExpense(
+        id: Long,
+        amount: Double,
+        category: ExpenseCategoryModel,
+        date: LocalDate,
+        note: String?
+    ) {
+        dao.update(
+            mapper.fromEntity(
+                ExpenseModel(
+                    id = id,
+                    amount = amount,
+                    category = category,
+                    date = date,
+                    note = note
+                )
+            )
+        )
+    }
+
+    override suspend fun changeAllExpenseCategoryByOldCategory(
+        old: String,
+        new: String
+    ) {
+        dao.changeAllExpenseCategoryByOldCategory(old, new)
+    }
+
+    override suspend fun deleteAllExpensesByCategory(category: String) {
+        dao.deleteAllExpensesByCategory(category)
+    }
+
+    override suspend fun deleteExpense(id: Long) {
+        dao.deleteById(id)
+    }
+
+    override fun getExpensesByDateRangeFlow(
+        startDate: LocalDate,
+        endDate: LocalDate
+    ): Flow<List<ExpenseModel>> {
+        return dao.getExpensesByDateRangeFlow(localDateToInt(startDate), localDateToInt(endDate))
+            .map { dbos -> dbos.map { mapper.toEntity(it) } }
+    }
+
+    override suspend fun getExpensesByDateRange(
+        startDate: LocalDate,
+        endDate: LocalDate
+    ): List<ExpenseModel> {
+        return dao.getExpensesByDateRange(localDateToInt(startDate), localDateToInt(endDate))
+            .map { mapper.toEntity(it) }
+    }
+}
