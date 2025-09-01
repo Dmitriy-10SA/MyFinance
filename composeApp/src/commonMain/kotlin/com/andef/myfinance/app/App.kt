06795984632c87a24a65956cfd05ea824902d7f5
dsemkin <semkin_dmitriy10@vk.com>
2025-09-01 10:24:02 -0700

    Добавил Полгода в табы топ бара

--- composeApp/src/commonMain/kotlin/com/andef/myfinance/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/andef/myfinance/app/App.kt
@@ -275,6 +275,7 @@ private val dateTabs = listOf(
     UiTopBarTab(id = 0, title = "День"),
     UiTopBarTab(id = 1, title = "Неделя"),
     UiTopBarTab(id = 2, title = "Месяц"),
-    UiTopBarTab(id = 3, title = "Год"),
-    UiTopBarTab(id = 4, title = "Период")
+    UiTopBarTab(id = 3, title = "Полгода"),
+    UiTopBarTab(id = 4, title = "Год"),
+    UiTopBarTab(id = 5, title = "Период")
 )
