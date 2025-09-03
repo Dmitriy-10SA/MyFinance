6a599d7e22f3690988e584b4713d4018dca3a7b2
dsemkin <semkin_dmitriy10@vk.com>
2025-09-02 22:02:34 -0700

    Date и Time пикеры для Android готовы

--- composeApp/src/commonMain/kotlin/com/andef/myfinance/core/design/time/picker/ui/UiTimePickerDialog.kt
+++ composeApp/src/commonMain/kotlin/com/andef/myfinance/core/design/time/picker/ui/UiTimePickerDialog.kt
@@ -1,15 +1,12 @@
 package com.andef.myfinance.core.design.time.picker.ui
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import kotlinx.datetime.LocalTime
 
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun UiTimePickerDialog(
-//    isVisible: Boolean,
-//    isLightTheme: Boolean,
-//    startTime: LocalTime = LocalTime.now(),
-//    onDismissRequest: () -> Unit,
-//    onOkClick: (LocalTime) -> Unit
-//)
+@Composable
+expect fun UiTimePickerDialog(
+    isVisible: Boolean,
+    isLightTheme: Boolean,
+    onDismissRequest: () -> Unit,
+    onOkClick: (LocalTime) -> Unit
+)
