fbd6c060923b3e8b51c9808d9574114ef2f4e73a
dsemkin <semkin_dmitriy10@vk.com>
2025-09-04 23:53:49 -0700

    C IOS все

--- composeApp/src/commonMain/kotlin/com/andef/myfinance/feature/currency/presentation/CurrencysScreen.kt
+++ composeApp/src/commonMain/kotlin/com/andef/myfinance/feature/currency/presentation/CurrencysScreen.kt
@@ -1,10 +1,8 @@
 package com.andef.myfinance.feature.currency.presentation
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,7 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.andef.myfinance.core.design.alert.dialog.ui.UiAlertDialog
 import com.andef.myfinance.core.design.card.currency.ui.UiCurrencyCard
