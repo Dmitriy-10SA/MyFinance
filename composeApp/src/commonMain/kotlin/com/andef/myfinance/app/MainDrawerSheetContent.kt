4afb843795bfde8c44c92c44c2239e9c1578fe5a
dsemkin <semkin_dmitriy10@vk.com>
2025-09-09 12:57:48 -0700

    Баннер в DrawerSheet готов

--- composeApp/src/commonMain/kotlin/com/andef/myfinance/app/MainDrawerSheetContent.kt
+++ composeApp/src/commonMain/kotlin/com/andef/myfinance/app/MainDrawerSheetContent.kt
@@ -44,6 +44,8 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.andef.myfinance.core.design.ads.type.UiAdsType
+import com.andef.myfinance.core.design.ads.ui.UiAds
 import com.andef.myfinance.core.design.bottom.sheet.ui.UiConnectionModalBottomSheet
 import com.andef.myfinance.core.design.bottom.sheet.ui.UiModalBottomSheet
 import com.andef.myfinance.core.design.button.ui.UiButton
@@ -210,6 +212,7 @@ private fun InnerContent(
             thickness = 1.dp,
             color = blackOrWhiteColor(isLightTheme).copy(alpha = 0.2f)
         )
+        UiAds(modifier = Modifier.fillMaxWidth(), type = UiAdsType.StickyBanner)
         LazyColumn(
             modifier = Modifier.weight(1f),
             horizontalAlignment = Alignment.Start
